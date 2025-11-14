937e5146630a10ec71e9a9f2a5cca97651d73a27
Mazen Naji <mazen.naji.1@gmail.com>
2025-11-14 16:04:29 +0200

    feat: add Battery class for calculating battery life and charge time

---
+++ lib/battery.dart
@@ -0,0 +1,18 @@
+class Battery {
+  double getBatteryLife(double capacity,double normalDrain,List<Drainer> drainers) {
+
+    double totalDrain = normalDrain; // start with the normal drain amount
+
+    // Add the extra drain from selected items
+    for (int i = 0; i < drainers.length; i++) {
+      if (drainers[i].isSelected) { // If a drainer is turned ON
+        totalDrain += drainers[i].extraDrain; // Add its extra drain to totalDrain
+      }
+    }
+    return capacity / totalDrain;;//Battery life = how much battery you have ÷ how fast it's draining
+  }
+
+  double getChargeTime(double capacity,double chargeSpeed) {
+    return capacity / chargeSpeed;// Charge time = how much battery you have ÷ how fast it charges
+  }
+}
